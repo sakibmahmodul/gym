43a2adad111b691dfd6911eb419f7758da673f0c
sakibmahmodul <sakibuddessho89@gmail.com>
2025-05-01 22:41:49 +0600

    Work with Navbar and hero section with responsiveness

--- src/main.js
+++ src/main.js
@@ -1,24 +1,14 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+const navToggle = document.getElementById('navToggle');
+const navLinks = document.getElementById('navLinks');
+navToggle.addEventListener('click', () => {
+  navToggle.classList.toggle('active');
+  navLinks.classList.toggle('active');
+}
+);
+// Close the menu when a link is clicked
+document.querySelectorAll('.nav-links a').forEach(link =>  {
+    link.addEventListener("click", () => {
+        navToggle.classList.remove('active')
+        navLinks.classList.remove('active')
+    })
+})
